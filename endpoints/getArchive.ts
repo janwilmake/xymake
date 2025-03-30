94682ba5eebd7bf0766fc21061a740edc5e8811e
Jan Wilmake <jan@wilmake.com>
2025-03-30 14:39:19 +0200

    improvements

--- endpoints/getArchive.ts
+++ endpoints/getArchive.ts
@@ -1,12 +1,23 @@
+import { getFormat } from "../getFormat.js";
 import { Env } from "../xLoginMiddleware.js";
 
 export const getArchive = async (request: Request, env: Env, ctx: any) => {
   const url = new URL(request.url);
   const [first] = url.pathname.split("/").slice(1);
   const username = first.split(".")[0];
+  const format = getFormat(request);
+  if (!format) {
+    return new Response("Unsupported format", { status: 400 });
+  }
+
   // Build zip simply by dereferencing all the JSON starting from `janwilmake.json`
-  // const headers = {Authorization:`Bearer ${env.ZIPOBJECT_API_KEY}`}
-  const headers = undefined;
+  const headers = {
+    Authorization: `Bearer ${env.ZIPOBJECT_API_KEY}`,
+    accept: format,
+  };
+  // TODO:
+  // 1) Finish zipobject $ref parsing
+  // 2) Finish zipobject html response
   return fetch(`https://zipobject.com/xymake.com/${username}.json`, {
     headers,
   });
